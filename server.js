8ac77ab728b87d2775586f5ec4db9f19ca1de06d
jbh2032 <jbh2032@naver.com>
2024-09-12 00:03:55 +0900

    last

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("예지야진심으로사랑해!!");
+    res.send("yeji i love you");
 })
 
 app.listen(3000,()=>{console.log("serveron")});
