379dc30daccb4718be1802574b0c40e006662b85
jbh2032 <jbh2032@naver.com>
2024-09-14 21:09:52 +0900

    테스트2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("yeji i love you!");
+    res.send("황예지님은 저에게는 예수,부처,알라 그이상의 존재이십니다.");
 })
 
 app.listen(3000,()=>{console.log("serveron")});
