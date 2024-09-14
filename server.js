b1690231917644d6cb345056c47482151ae140a0
jbh2032 <jbh2032@naver.com>
2024-09-14 21:07:55 +0900

    '테스트'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("yeji i love you");
+    res.send("yeji i love you!");
 })
 
 app.listen(3000,()=>{console.log("serveron")});
